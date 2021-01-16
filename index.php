d00abeabf1de0e9d64cdf65e09a385d74d24a5df
jaeparc <tgarnon45@gmail.com>
2021-01-16 12:10:44 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+    <body>
+        <div class="container">
+            <H1 class="center-align">NBA Manager 21</H1>
+            <div class="row center-align">
+                <div class="col s4">
+                    <a class="waves-effect waves-light btn">Nouvelle partie</a>
+                </div>
+                <div class="col s4">
+                    <a class="waves-effect waves-light btn">Charger partie</a>
+                </div>
+                <div class="col s4">
+                    <a class="waves-effect waves-light btn">Crédits</a>
+                </div>
+            </div>
+        </div>
+
+        <script type="text/javascript" src="js/materialize.min.js"></script>
+    </body>
+</html>
---
+++ newgame.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+    <body>
+        <div class="container">
+            <H1 class="center-align">NBA Manager 21</H1>
+            <h5>Nouvelle partie</h5>
+        </div>
+
+        <script type="text/javascript" src="js/materialize.min.js"></script>
+    </body>
+</html>
